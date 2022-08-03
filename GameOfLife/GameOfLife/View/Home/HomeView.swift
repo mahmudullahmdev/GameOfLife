9253e6f8562ada4a40bc4156ea190357ea89f4d9
Mahmudullah <mahmudullahmdev@gmail.com>
2022-08-03 23:58:56 +0600

    All the rules applied properly . Showing generations as text on top, A next button is on top to change the generations by repeating  rules on user interaction .

--- GameOfLife/GameOfLife/View/Home/HomeView.swift
+++ GameOfLife/GameOfLife/View/Home/HomeView.swift
@@ -11,82 +11,68 @@ struct HomeView: View {
     var items: [GridItem] = []
     @ObservedObject  var viewModel = HomeViewModel()
     @State var seedPlaced: Bool = false
+    @State var generation: Int = 1
     var body: some View {
         GeometryReader {proxy in
             ScrollView {
                 VStack {
+                    // Start of Top Portion of the screen
                     HStack(alignment: .center) {
-                        Button(action: {
-                            self.viewModel.placeInitialSeed(row: Int(proxy.size.height + proxy.size.width/20), column: Int(proxy.size.width/24) )
-                            self.seedPlaced = true
-                            
-                        }){
-                            Text("Place Seed")
-                                .padding(.all, 6)
-                                .foregroundColor(.white)
-                        }
-                        .background(Color(UIColor.blue))
-                        .cornerRadius(5)
+                        Text( self.generation > 1 ?  "Generation: \(self.generation)" : "Tap in the cells and place your first generation" )
+                            .foregroundColor(Color.accentColor)
                         
+                        // Start and generate next generations by tapping this button.
+                        // Only start game when seed is placed to the chart..
                         Button(action: {
                             if self.seedPlaced {
-                                // start game
+                                self.generation += 1
                                 self.viewModel.startGame()
                             }
                         }){
-                            Text("Start")
+                            Text(self.seedPlaced ? "Next" : "Start")
                                 .padding(.all, 6)
                                 .foregroundColor(.white)
                         }
                         .background(Color(UIColor.blue))
                         .cornerRadius(5)
                     }
-                    .padding(.top, 6)
+                    .padding(.top, 6)  // End of Top Portion of the screen
+
                     ScrollView(.vertical, showsIndicators: false) {
-                        
+                        // This stack generates the coloumns
                         LazyVStack(spacing: 0) {
                             ForEach(0..<viewModel.cellList.count, id: \.self) { y in
+                                
+                                // This stack generates the rows
                                 LazyHStack(spacing: 0) {
                                     ForEach(0..<viewModel.cellList[y].count, id: \.self) { x in
                                         Button(action: {
                                             viewModel.cellList[y][x].isAlive.toggle()
-                                            print("x: \(x) y: \(y)")
-                                            self.seedPlaced = true
+                                            self.seedPlaced.toggle()
                                         }){
                                             Rectangle()
                                                 .foregroundColor(self.viewModel.cellList[y][x].isAlive ? .black : .white )
                                                 .frame(width: proxy.size.width/30, height:  proxy.size.width/30)
                                                 .border(.gray, width: 1)
                                         }
-                                      
+                                        
                                     }
                                 }
-                              
+                                
                             }
                         }
                         .padding(.leading, 6)
                         .padding(.trailing, 6)
                         .padding(.bottom, 6)
                     }
-                    
-                   
+  
                 }
             }
             .onAppear{
                 viewModel.initCells(rows: 144)
             }
-            
-            
-            
-            
+   
         }
         
     }
 }
-
-//struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView()
-//            .previewInterfaceOrientation(.landscapeLeft)
-//    }
-//}
--- GameOfLife/GameOfLife/ViewModel/HomeViewModel.swift
+++ GameOfLife/GameOfLife/ViewModel/HomeViewModel.swift
@@ -9,30 +9,29 @@ import Foundation
 class HomeViewModel : ObservableObject {
     @Published var cellList:[[CellModel]] = []
     
+    // Generate Columns and Rows for 12x12 matrix the playground
     func initCells(rows:Int) {
-        var parentList:[[CellModel]] = []
-        for _ in 0...12 {
-            var childLists: [CellModel] = []
-            for _ in 0...12 {
-                childLists.append(CellModel.init())
+        var listOfColoumn:[[CellModel]] = []
+        for _ in 0...11 {
+            var listOfRows: [CellModel] = []
+            for _ in 0...11 {
+                listOfRows.append(CellModel.init())
             }
-            parentList.append(childLists)
+            listOfColoumn.append(listOfRows)
         }
-        self.cellList = parentList
+        self.cellList = listOfColoumn
         
     }
     
-    func placeInitialSeed(row: Int ,column: Int) {
-        
-    }
-    
+    // This method starts the game and applies all the rules of GameOfLife
     func startGame() {
         var cellList = self.cellList
-        for y in 0..<cellList.count - 1 {
-            for x in 0..<cellList[y].count - 1 {
+        for y in 0..<cellList.count {
+            for x in 0..<cellList[y].count {
                 
-                let list = cellList[y]
-                let isAlive =  list[x].isAlive
+                // Take the positions of 8 cells around the target cell vertically
+                // horizontally and diagonally
+                let isAlive =  cellList[y][x].isAlive
                 
                 let topy = y - 1
                 let topx = x
@@ -58,77 +57,82 @@ class HomeViewModel : ObservableObject {
                 let diagBottomRighty = y + 1
                 let diagBottomRightx = x + 1
                 
+                
+                // Checkf if a cell is alive or not and increase the alive count for all cells of row
+                // Don't take any negative and > cellList count indexes.
                 var aliveCount: Int = 0
-                
-                
-                if  topy >= 0 && topx >= 0 {
+                if  topy >= 0 && topx >= 0 && topy < cellList.count && topx < cellList.count {
                     if cellList[topy][topx].isAlive {
                         aliveCount += 1
                     }
                 }
                 
-                if  bottomx >= 0 && bottomy >= 0 {
+                if  bottomx >= 0 && bottomy >= 0 && bottomx < cellList.count && bottomy < cellList.count {
                     if cellList[bottomy][bottomx].isAlive {
                         aliveCount += 1
                     }
                 }
-                if lefty >= 0 && leftx >= 0  {
+                if lefty >= 0 && leftx >= 0 && lefty < cellList.count && leftx < cellList.count {
                     if cellList[lefty][leftx].isAlive {
                         aliveCount += 1
                     }
                 }
-                if rightx >= 0 && righty >= 0 {
+                if rightx >= 0 && righty >= 0 && rightx < cellList.count && righty < cellList.count {
                     if cellList[righty][rightx].isAlive {
                         aliveCount += 1
                     }
                 }
                 
-                if  diagTopRighty >= 0 && diagTopRightx >= 0 {
+                if  diagTopRighty >= 0 && diagTopRightx >= 0 && diagTopRightx < cellList.count && diagTopRightx < cellList.count {
                     if cellList[diagTopRighty][diagTopRightx].isAlive {
                         aliveCount += 1
                     }
                 }
                 
-                if  diagTopLeftx >= 0 && diagTopLefty >= 0 {
+                if  diagTopLeftx >= 0 && diagTopLefty >= 0 && diagTopLeftx < cellList.count && diagTopLefty < cellList.count {
                     if cellList[diagTopLefty][diagTopLeftx].isAlive {
                         aliveCount += 1
                     }
                 }
                 
-                if diagBottomLeftx >= 0 && diagBottomLefty >= 0 {
+                if diagBottomLeftx >= 0 && diagBottomLefty >= 0 && diagBottomLeftx < cellList.count && diagBottomLefty < cellList.count {
                     if cellList[diagBottomLefty][diagBottomLeftx].isAlive {
                         aliveCount += 1
                     }
                 }
-                if  diagBottomRightx >= 0 && diagBottomRighty >= 0 {
+                if  diagBottomRightx >= 0 && diagBottomRighty >= 0 && diagBottomRightx < cellList.count && diagBottomRighty < cellList.count {
                     if cellList[diagBottomRighty][diagBottomRightx].isAlive {
                         aliveCount += 1
                     }
                 }
                 
-                
+                // We have now the total alive count of cell around neighbours
+                // Apply the rules now
                 if isAlive {
+                    // Any live cell with fewer then 2 live neighbors die, as if by underpopulation.
+                    // Any live cell with two or three live neighbors lives on the next generation.
+                    // Any live cell with more than three live neighbors dies, as if by overpopulation.
                     if aliveCount < 2 {
                         cellList[y][x].isAlive = false
-                        
                     } else if aliveCount >= 2 && aliveCount <= 3 {
                         cellList[y][x].isAlive = true
                     } else if aliveCount > 3 {
                         cellList[y][x].isAlive = false
-                        
                     }
                     
                 } else {
+                    // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
                     if aliveCount == 3 {
-                        cellList[x][y].isAlive = true
-                       
+                        cellList[y][x].isAlive = true
+                        
                     }
                 }
-
+                
             }
             self.cellList = cellList
+            
         }
-   
+        
         
     }
     
